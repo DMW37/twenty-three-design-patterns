7bd386bb0b7ab6769b7dff337928474d90408794
邓明维 <3561294402@qq.com>
2022-09-07 16:22:34 +0800

    23种设计模式--结构型--命令模式

---
+++ src/main/java/com/dmw/functional/command/Command.java
@@ -0,0 +1,5 @@
+package com.dmw.functional.command;
+
+public interface Command {
+    void execute();//只需要定义一个统一的执行方法
+}
---
+++ src/main/java/com/dmw/functional/command/SeniorChef.java
@@ -0,0 +1,9 @@
+package com.dmw.functional.command;
+
+// 资深大厨类 是命令的Receiver
+public class SeniorChef {
+
+    public void makeFood(int num,String foodName) {
+        System.out.println(num + "份" + foodName);
+    }
+}
