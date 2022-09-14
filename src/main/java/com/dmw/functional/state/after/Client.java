f30c140600dd2ea37e4b7172b27069c920bdadb8
邓明维 <3561294402@qq.com>
2022-09-14 19:25:05 +0800

    23种设计模式--功能型--状态模式

---
+++ src/main/java/com/dmw/functional/state/after/Client.java
@@ -0,0 +1,14 @@
+package com.dmw.functional.state.after;
+
+//测试类
+public class Client {
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.setLiftState(new ClosingState());
+
+        context.open();
+        context.close();
+        context.run();
+        context.stop();
+    }
+}
